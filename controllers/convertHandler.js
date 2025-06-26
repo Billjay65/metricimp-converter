d4986f76b1d338663b8921e127eff22b3895e8f2
Sikeh Beryuf <greatmbua@yahoo.com>
2025-06-26 13:38:21 +0000

    feat: updated convertHandler and passed first 6 tests units

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,9 +1,38 @@
 function ConvertHandler() {
-  
+  // get submitted input string and return number
   this.getNum = function(input) {
     let result;
-    // get submitted string and return number
-    return result;
+
+    // match different types of input
+    // match whole and decimal numbers
+    let matchNumber = input.match(/^(\d*\.?\d+)(?=[a-zA-Z])/);
+    let resultNumber = matchNumber ? parseFloat(matchNumber[1]) : null;
+
+    // match fractional input 
+    // Match fractional input like "1/2mi"
+    const matchFraction = input.match(/^(\d+\/\d+)(?=[a-zA-Z])/);
+    const resultFraction = matchFraction ? matchFraction[1] : null;
+    
+    // match double-fraction
+    matchDoubleFrac = input.match(/^(\d+\/\d+\/\d+)(?=[a-zA-Z])/)
+    let resultDoubleFrac = matchDoubleFrac ? matchDoubleFrac[1] : null;
+
+    // return result based on input matched
+    if (resultNumber) {
+      result = resultNumber;
+      return result;
+    } else if (resultFraction) {
+      result = resultFraction;
+      return result
+    } else if (resultDoubleFrac) {
+      result = 'Error: double fraction number';
+      return result;
+    } else {
+      // return default of 1 when no numerical
+      // input is provided
+      result = 1;
+      return result;
+    }
   };
   
   this.getUnit = function(input) {
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,35 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
-
+  // #1
+  test('Read a whole number input', function () {
+    assert.equal(convertHandler.getNum('12mi'), 12, 
+      'Whole Number is returned from input string');
+  });
+  // #2
+  test('Read a decimal number input', function () {
+    assert.equal(convertHandler.getNum('12.34mi'), 12.34, 
+      'Decimal number is returned from input string');
+  });
+  // #3
+  test('Read fractional input', function () {
+    assert.equal(convertHandler.getNum('1/2mi'), '1/2',
+      'Fraction is returned from input string');
+  });
+  // #4
+  test('Read a fractional input with a decimal', function () {
+    assert.equal(convertHandler.getNum('0.5km'), '0.5',
+      'Decimal fractional input is returned from input string')
+  });
+  // #5
+  test('Return error on a double-fraction', function () {
+    assert.equal(convertHandler.getNum('3/45/42km'), 
+      'Error: double fraction number',
+      'Error is returned if input is double-fraction');
+  });
+  // #6
+  test('Return default input 1 when no numerical input', function () {
+    assert.equal(convertHandler.getNum('d34'), 
+      1, '1 is returned if input is not numeric');
+  });
 });
